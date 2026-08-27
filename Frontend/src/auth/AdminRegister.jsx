07ce60e6118c7070382622fb428060ccf94b5bef
Koshal765 <koshalpothare@gmail.com>
2026-08-27 15:38:39 +0530

    fixed import of admin

--- Frontend/src/auth/AdminRegister.jsx
+++ Frontend/src/auth/AdminRegister.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {Eye,EyeOff} from 'lucide-react'
-import { API_BASE } from '../../Api';
+import { API_BASE } from '../services/api';
 
 const AdminRegister = () => {
   const navigate = useNavigate();
